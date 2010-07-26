6c9c5d3f1fdb9f549c087573dcb59ada7fe38846
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2010-07-26 21:23:30 +0430

    GUI's stub is stable
    This is what GUI will look like after the application is finished.

--- src/com/github/mfs/frt/ui/MainFrame.java
+++ src/com/github/mfs/frt/ui/MainFrame.java
@@ -1,14 +1,21 @@
 /**
- * File Rename Tool Copyright (C) 2010, Meisam Fathi <meisam.fathi[at]gmail.com>
+ * File Rename Tool
+ * Copyright (C) 2010, Meisam Fathi <meisam.fathi[at]gmail.com>
+ * 
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version. This library is distributed in the hope that it will be
- * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
- * General Public License for more details. You should have received a copy of
- * the GNU Lesser General Public License along with this library; if not, write
- * to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library;
+ * if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  */
 
@@ -16,12 +23,18 @@ package com.github.mfs.frt.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.HeadlessException;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.ListModel;
 
 /**
  * @author Meisam Fathi
@@ -62,7 +75,17 @@ public class MainFrame extends JFrame {
   /**
    * 
    */
+  private JLabel prefixLabel;
+
+  /**
+   * 
+   */
   private JTextField prefixField;
+
+  /**
+   * 
+   */
+  private JLabel postfixLabel;
 
   /**
    * 
@@ -73,6 +96,14 @@ public class MainFrame extends JFrame {
 	 * 
 	 */
   private JButton exportButtton;
+
+  private JLabel fileNameLabel;
+
+  private JScrollPane scrollPane;
+
+  private JList filesList;
+
+  private DefaultListModel filesListDataModel;
 
   /**
    * @throws HeadlessException
@@ -127,8 +158,37 @@ public class MainFrame extends JFrame {
   private JPanel getFilesListPanel() {
     if (this.filesListPanel == null) {
       this.filesListPanel = new JPanel();
+      this.filesListPanel.setLayout(new BorderLayout());
+      this.filesListPanel.add(getScrollPane(), BorderLayout.CENTER);
     }
     return this.filesListPanel;
+  }
+
+  private JScrollPane getScrollPane() {
+    if (this.scrollPane == null) {
+      this.scrollPane = new JScrollPane();
+      this.scrollPane.setPreferredSize(new Dimension(600, 500));
+      this.scrollPane.getViewport().add(getFilesList());
+    }
+    return this.scrollPane;
+  }
+
+  private JList getFilesList() {
+    if (this.filesList == null) {
+      this.filesList = new JList(getFilesListDataModel());
+    }
+    return this.filesList;
+  }
+
+  private DefaultListModel getFilesListDataModel() {
+    if (this.filesListDataModel == null) {
+      this.filesListDataModel = new DefaultListModel();
+      this.filesListDataModel.add(0, "Hello");
+      this.filesListDataModel.add(0, "Hello");
+      this.filesListDataModel.add(0, "Hello");
+      this.filesListDataModel.add(0, "Hello");
+    }
+    return this.filesListDataModel;
   }
 
   /**
@@ -139,6 +199,9 @@ public class MainFrame extends JFrame {
       this.actionsPanel = new JPanel();
       this.actionsPanel.add(getExportButton());
       this.actionsPanel.add(getPrefixField());
+      this.actionsPanel.add(getPrefixLabel());
+      this.actionsPanel.add(getFileNameLabel());
+      this.actionsPanel.add(getPostLabel());
       this.actionsPanel.add(getPostfixField());
     }
     return this.actionsPanel;
@@ -147,9 +210,29 @@ public class MainFrame extends JFrame {
   /**
    * @return
    */
+  private JLabel getPrefixLabel() {
+    if (this.prefixLabel == null) {
+      this.prefixLabel = new JLabel(" + ");
+    }
+    return this.prefixLabel;
+  }
+
+  /**
+   * @return
+   */
+  private JLabel getFileNameLabel() {
+    if (this.fileNameLabel == null) {
+      this.fileNameLabel = new JLabel(" file ");
+    }
+    return this.fileNameLabel;
+  }
+
+  /**
+   * @return
+   */
   private JTextField getPrefixField() {
     if (this.prefixField == null) {
-      this.prefixField = new JTextField(50);
+      this.prefixField = new JTextField(30);
     }
     return this.prefixField;
   }
@@ -157,9 +240,19 @@ public class MainFrame extends JFrame {
   /**
    * @return
    */
+  private JLabel getPostLabel() {
+    if (this.postfixLabel == null) {
+      this.postfixLabel = new JLabel(" + ");
+    }
+    return this.postfixLabel;
+  }
+
+  /**
+   * @return
+   */
   private JTextField getPostfixField() {
     if (this.postfixField == null) {
-      this.postfixField = new JTextField(50);
+      this.postfixField = new JTextField(30);
     }
     return this.postfixField;
   }
